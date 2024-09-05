5307f4614ec4c4ccf5db5763c3052374d8eeb604
dreammehedi <dreammehedihassan@gmail.com>
2024-09-05 16:27:06 +0600

    Account Setting Bottom Complete

--- src/dashboard_pages/pages-profile/PagesSetting.jsx
+++ src/dashboard_pages/pages-profile/PagesSetting.jsx
@@ -1,3 +1,4 @@
+import { FaEye } from "react-icons/fa";
 import {
   MdOutlineEmail,
   MdOutlineLocationOn,
@@ -64,7 +65,7 @@ function PagesSetting() {
               <div className="flex flex-col gap-2">
                 <label htmlFor="name">Name</label>
                 <input
-                  className="outline-none p-[10px] rounded-md text-[#959FA3] text-sm ring-1 ring-[#959FA3]"
+                  className="outline-none p-[10px] rounded-md text-[#959FA3] text-sm ring-1 ring-[#959FA3] w-full"
                   type="text"
                   placeholder="Full Name"
                   id="name"
@@ -74,7 +75,7 @@ function PagesSetting() {
               <div className="flex flex-col gap-2">
                 <label htmlFor="title">Title</label>
                 <input
-                  className="outline-none p-[10px] rounded-md text-[#959FA3] text-sm ring-1 ring-[#959FA3]"
+                  className="outline-none p-[10px] rounded-md text-[#959FA3] text-sm ring-1 ring-[#959FA3] w-full"
                   type="text"
                   placeholder="Headline, Sub Title"
                   id="title"
@@ -118,7 +119,7 @@ function PagesSetting() {
                 <select
                   name="team"
                   id="team"
-                  className="outline-none p-[10px] rounded-md text-[#959FA3] text-sm ring-1 ring-[#959FA3]"
+                  className="outline-none p-[10px] rounded-md text-[#959FA3] text-sm ring-1 ring-[#959FA3] w-full"
                 >
                   <option value="">Select...</option>
                   <option value="team a">Team A</option>
@@ -131,7 +132,7 @@ function PagesSetting() {
                 <select
                   name="country"
                   id="country"
-                  className="outline-none p-[10px] rounded-md text-[#959FA3] text-sm ring-1 ring-[#959FA3]"
+                  className="outline-none p-[10px] rounded-md text-[#959FA3] text-sm ring-1 ring-[#959FA3] w-full"
                 >
                   <option value="">Select...</option>
                   <option value="bangladesh">Bangladesh</option>
@@ -144,7 +145,7 @@ function PagesSetting() {
                 <select
                   name="city"
                   id="city"
-                  className="outline-none p-[10px] rounded-md text-[#959FA3] text-sm ring-1 ring-[#959FA3]"
+                  className="outline-none p-[10px] rounded-md text-[#959FA3] text-sm ring-1 ring-[#959FA3] w-full"
                 >
                   <option value="">Select...</option>
                   <option value="dhaka">Dhaka</option>
@@ -155,7 +156,7 @@ function PagesSetting() {
               <div className="lg:col-span-2 flex flex-col gap-2">
                 <label htmlFor="dateOfBirth">Date of Birth</label>
                 <input
-                  className="outline-none p-[10px] rounded-md text-[#959FA3] text-sm ring-1 ring-[#959FA3]"
+                  className="outline-none p-[10px] rounded-md text-[#959FA3] text-sm ring-1 ring-[#959FA3] w-full"
                   type="date"
                   id="dateOfBirth"
                   name="dateOfBirth"
@@ -166,7 +167,7 @@ function PagesSetting() {
                 <select
                   name="language"
                   id="language"
-                  className="outline-none p-[10px] rounded-md text-[#959FA3] text-sm ring-1 ring-[#959FA3]"
+                  className="outline-none p-[10px] rounded-md text-[#959FA3] text-sm ring-1 ring-[#959FA3] w-full"
                 >
                   <option value="">Select...</option>
                   <option value="english">English</option>
@@ -196,7 +197,7 @@ function PagesSetting() {
               <div className="flex flex-col gap-2">
                 <label htmlFor="facebook">Facebook</label>
                 <input
-                  className="outline-none p-[10px] rounded-md text-[#959FA3] text-sm ring-1 ring-[#959FA3]"
+                  className="outline-none p-[10px] rounded-md text-[#959FA3] text-sm ring-1 ring-[#959FA3] w-full"
                   type="text"
                   placeholder="Facebook profile url..."
                   id="facebook"
@@ -206,7 +207,7 @@ function PagesSetting() {
               <div className="flex flex-col gap-2">
                 <label htmlFor="twitter">Twitter</label>
                 <input
-                  className="outline-none p-[10px] rounded-md text-[#959FA3] text-sm ring-1 ring-[#959FA3]"
+                  className="outline-none p-[10px] rounded-md text-[#959FA3] text-sm ring-1 ring-[#959FA3] w-full"
                   type="text"
                   placeholder="Twitter profile url..."
                   id="twitter"
@@ -216,7 +217,7 @@ function PagesSetting() {
               <div className="flex flex-col gap-2">
                 <label htmlFor="linkedin">Linkedin</label>
                 <input
-                  className="outline-none p-[10px] rounded-md text-[#959FA3] text-sm ring-1 ring-[#959FA3]"
+                  className="outline-none p-[10px] rounded-md text-[#959FA3] text-sm ring-1 ring-[#959FA3] w-full"
                   type="text"
                   placeholder="Linkedin profile url..."
                   id="linkedin"
@@ -226,7 +227,7 @@ function PagesSetting() {
               <div className="flex flex-col gap-2">
                 <label htmlFor="instagram">Instagram</label>
                 <input
-                  className="outline-none p-[10px] rounded-md text-[#959FA3] text-sm ring-1 ring-[#959FA3]"
+                  className="outline-none p-[10px] rounded-md text-[#959FA3] text-sm ring-1 ring-[#959FA3] w-full"
                   type="text"
                   placeholder="Instagram profile url..."
                   id="instagram"
@@ -256,7 +257,7 @@ function PagesSetting() {
                 <div className="relative">
                   <MdOutlinePhoneInTalk className="absolute top-1/2 -translate-y-1/2 left-2 text-dark text-lg"></MdOutlinePhoneInTalk>
                   <input
-                    className="outline-none p-[10px] pl-[30px] rounded-md text-[#959FA3] text-sm ring-1 ring-[#959FA3]"
+                    className="outline-none p-[10px] pl-[30px] rounded-md text-[#959FA3] text-sm ring-1 ring-[#959FA3] w-full"
                     type="number"
                     placeholder="Phone number"
                     id="phone"
@@ -269,7 +270,7 @@ function PagesSetting() {
                 <div className="relative">
                   <MdOutlineEmail className="absolute top-1/2 -translate-y-1/2 left-2 text-dark text-lg"></MdOutlineEmail>
                   <input
-                    className="outline-none p-[10px] pl-[30px] rounded-md text-[#959FA3] text-sm ring-1 ring-[#959FA3]"
+                    className="outline-none p-[10px] pl-[30px] rounded-md text-[#959FA3] text-sm ring-1 ring-[#959FA3] w-full"
                     type="email"
                     placeholder="Email address"
                     id="email"
@@ -282,7 +283,7 @@ function PagesSetting() {
                 <div className="relative">
                   <MdOutlineLocationOn className="absolute top-1/2 -translate-y-1/2 left-2 text-dark text-lg"></MdOutlineLocationOn>
                   <input
-                    className="outline-none p-[10px] pl-[30px] rounded-md text-[#959FA3] text-sm ring-1 ring-[#959FA3]"
+                    className="outline-none p-[10px] pl-[30px] rounded-md text-[#959FA3] text-sm ring-1 ring-[#959FA3] w-full"
                     type="location"
                     placeholder="Current location"
                     id="location"
@@ -295,7 +296,7 @@ function PagesSetting() {
                 <div className="relative">
                   <TiShoppingBag className="absolute top-1/2 -translate-y-1/2 left-2 text-dark text-lg"></TiShoppingBag>
                   <input
-                    className="outline-none p-[10px] pl-[30px] rounded-md text-[#959FA3] text-sm ring-1 ring-[#959FA3]"
+                    className="outline-none p-[10px] pl-[30px] rounded-md text-[#959FA3] text-sm ring-1 ring-[#959FA3] w-full"
                     type="text"
                     placeholder="Personal portfolio url"
                     id="portfolio"
@@ -310,6 +311,189 @@ function PagesSetting() {
                 Save Changes
               </button>
             </form>
+          </div>
+        </div>
+
+        {/* account setting bottom */}
+        <div className="grid grid-cols-12 gap-4 md:gap-6 lg:gap-8">
+          {/* change password */}
+          <div className="lg:col-span-4 bg-primary rounded-md h-fit">
+            {/* title */}
+            <p className="px-4 md:px-5 lg:px-6 py-3 text-dark font-medium border-b border-[#959FA3]">
+              Change Password
+            </p>
+
+            {/* form */}
+            <form className="w-full p-4 md:p-5 lg:p-6 grid grid-cols-1 gap-4 md:gap-6">
+              <div className="flex flex-col gap-2">
+                <label htmlFor="currentPassword">Current Password</label>
+                <div className="relative">
+                  <FaEye className="absolute top-1/2 -translate-y-1/2 right-2 text-dark text-lg"></FaEye>
+                  <input
+                    className="outline-none p-[10px] rounded-md text-[#959FA3] text-sm ring-1 ring-[#959FA3] w-full"
+                    type="password"
+                    placeholder="Password"
+                    id="currentPassword"
+                    name="currentPassword"
+                  />
+                </div>
+              </div>
+              <div className="flex flex-col gap-2">
+                <label htmlFor="newPassword">New Password</label>
+                <div className="relative">
+                  <FaEye className="absolute top-1/2 -translate-y-1/2 right-2 text-dark text-lg"></FaEye>
+                  <input
+                    className="outline-none p-[10px] rounded-md text-[#959FA3] text-sm ring-1 ring-[#959FA3] w-full"
+                    type="password"
+                    placeholder="Password"
+                    id="newPassword"
+                    name="newPassword"
+                  />
+                </div>
+              </div>
+              <div className="flex flex-col gap-2">
+                <label htmlFor="confirmPassword">Confirm Password</label>
+                <div className="relative">
+                  <FaEye className="absolute top-1/2 -translate-y-1/2 right-2 text-dark text-lg"></FaEye>
+                  <input
+                    className="outline-none p-[10px] rounded-md text-[#959FA3] text-sm ring-1 ring-[#959FA3] w-full"
+                    type="password"
+                    placeholder="Password"
+                    id="confirmPassword"
+                    name="confirmPassword"
+                  />
+                </div>
+              </div>
+              <button
+                type="submit"
+                className="bg-text-color  text-sm font-semibold px-6 py-3 capitalize text-white rounded-full mr-auto my-transition hover:bg-text-color/10 hover:text-text-color"
+              >
+                Save Changes
+              </button>
+            </form>
+          </div>
+
+          {/* advanced setting */}
+          <div className="lg:col-span-3 bg-primary rounded-md h-fit">
+            {/* title */}
+            <p className="px-4 md:px-5 lg:px-6 py-3 text-dark font-medium border-b border-[#959FA3]">
+              Advanced Setting
+            </p>
+
+            {/* form */}
+            <form className="w-full p-4 md:p-5 lg:p-6 grid grid-cols-1 gap-4 md:gap-6 text-dark">
+              <div className="flex gap-2">
+                <input
+                  className="outline-none rounded-md text-sm "
+                  defaultChecked
+                  type="checkbox"
+                  name="showMyBalance"
+                />
+                <label htmlFor="showMyBalance">Show my balance</label>
+              </div>
+              <div className="flex gap-2">
+                <input
+                  className="outline-none rounded-md text-sm "
+                  defaultChecked
+                  type="checkbox"
+                  name="kanbanBoard"
+                />
+                <label htmlFor="kanbanBoard">
+                  Show total Kanban Board card
+                </label>
+              </div>
+              <div className="flex gap-2">
+                <input
+                  className="outline-none rounded-md text-sm "
+                  type="checkbox"
+                  name="calendarTask"
+                />
+                <label htmlFor="calendarTask">Show total Calendar task</label>
+              </div>
+              <div className="flex gap-2">
+                <input
+                  className="outline-none rounded-md text-sm "
+                  defaultChecked
+                  type="checkbox"
+                  name="calendarEvent"
+                />
+                <label htmlFor="calendarEvent">
+                  Show total Calendar events
+                </label>
+              </div>
+              <hr />
+              <div className="flex gap-2">
+                <input
+                  className="outline-none rounded-md text-sm "
+                  defaultChecked
+                  type="checkbox"
+                  name="privateInformation"
+                />
+                <label htmlFor="privateInformation">
+                  Show private information
+                </label>
+              </div>
+              <div className="flex gap-2">
+                <input
+                  className="outline-none rounded-md text-sm "
+                  type="checkbox"
+                  name="contactInformation"
+                />
+                <label htmlFor="contactInformation">Contact information</label>
+              </div>
+              <div className="flex gap-2">
+                <input
+                  className="outline-none rounded-md text-sm "
+                  type="checkbox"
+                  name="socialMediaLink"
+                />
+                <label htmlFor="socialMediaLink">Social media link</label>
+              </div>
+              <button
+                type="submit"
+                className="bg-text-color  text-sm font-semibold px-6 py-3 capitalize text-white rounded-full mr-auto my-transition hover:bg-text-color/10 hover:text-text-color"
+              >
+                Save Changes
+              </button>
+            </form>
+          </div>
+
+          {/* pricing plans */}
+          <div className="lg:col-span-5 bg-primary rounded-md space-y-4 h-fit">
+            {/* upgrade plane */}
+            <div className="p-4 md:p-5 lg:p-6 space-y-4">
+              {/* title */}
+              <h3 className="text-lg text-dark font-medium ">Pricing Plans</h3>
+              <div className="text-sm space-y-2 ">
+                <h4 className="text-[#005CE8]  uppercase">🚀 Premium</h4>
+                <p className="text-[#626C70]">
+                  Transfer this account to another user or to an organization
+                  where you have the ability to create repositories.
+                </p>
+              </div>
+              <button
+                type="submit"
+                className="bg-text-color  text-sm font-semibold px-6 py-3 capitalize text-white rounded-full mr-auto my-transition hover:bg-text-color/10 hover:text-text-color"
+              >
+                Upgrade Plane
+              </button>
+            </div>
+            <hr />
+            {/* dactivate account */}
+            <div className="px-4 md:px-5 lg:px-6 py-2 space-y-2">
+              <h3 className="text-lg text-dark font-medium ">
+                Deactivate this account
+              </h3>
+              <p>
+                Once you delete a account, there is no going back. Please be
+                certain.
+              </p>
+              <button
+                className={`bg-[#FDEDED] text-[#E84646] capitalize text-sm font-semibold px-6 lg:px-8 py-2 rounded-full my-transition  ring-2 ring-transparent hover:bg-transparent hover:ring-[#E84646]`}
+              >
+                Deactivated account
+              </button>
+            </div>
           </div>
         </div>
       </section>
